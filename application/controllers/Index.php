3429be60351a5e4d03bc09fa2077bd783edea4cd
Jlake Ou <ouzhiwei@gmail.com>
2013-01-23 23:11:28 +0900

    add class TestModel

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -4,7 +4,9 @@ class IndexController extends Yaf_Controller_Abstract
    public function indexAction()
    {
         $this->_view->name = 'Yet Another Framework';
-        $this->_view->msg = 'Hello World';
+        //$this->_view->msg = 'Hello World';
+		$test = new TestModel();
+		$this->_view->msg = $test->getMessage();
    }
 
    public function dbtestAction()
---
+++ application/models/Test.php
@@ -0,0 +1,8 @@
+<?php
+class TestModel
+{
+	public function getMessage()
+	{
+		return "message from TestModel";
+	}
+}
